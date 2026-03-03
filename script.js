9d977f24a7fc6c08edd651523fd22ceed16b9fb9
k13r0n <kieronbellis95@gmail.com>
2026-03-03 21:39:39 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+function changeColor() {
+    document.body.classList.toggle("alt-theme");
+}
+
+const music = document.getElementById("bg-music");
+const button = document.getElementById("music-btn");
+
+const playlist = [
+    "Two Six.mp3"
+];
+
+let currentSong = 0;
+
+window.addEventListener("load", () => {
+    music.src = playlist[currentSong];
+});
+
+
+button.addEventListener("click", () => {
+    if (music.paused) {
+        music.volume = 0.5; 
+        music.play().then(() => {
+            button.textContent = "⏸";
+        }).catch(err => {
+            console.log("Playback blocked until user interaction");
+        })
+        music.play().catch(() => {
+            console.log("Autoplay blocked until user interaction");
+        });
+        button.textContent = "⏸";
+    } else {
+        music.pause();
+        button.textContent = "🎵";
+    }
+});
+
+music.addEventListener("ended", () => {
+    currentSong++;
+
+    if (currentSong >= playlist.length) {
+        currentSong = 0;
+    }
+
+    music.src = playlist[currentSong];
+    music.play();
+});
